3a6afd6ad10a0b64bc7053ec7ff0222d311a46f0
Manish Goregaokar <manishsmail@gmail.com>
2019-04-23 11:20:03 -0700

    Add mutable arena example

---
+++ examples/mutable_arena.rs
@@ -0,0 +1,61 @@
+use elsa::FrozenVec;
+
+fn main() {
+    let arena = Arena::new();
+    let lonely = arena.add_thing("lonely", vec![]);
+    let best_friend = arena.add_thing("best friend", vec![lonely]);
+    let threes_a_crowd = arena.add_thing("threes a crowd", vec![lonely, best_friend]);
+    let rando = arena.add_thing("rando", vec![]);
+    let facebook = arena.add_thing("facebook", vec![rando, threes_a_crowd, lonely, best_friend]);
+    arena.dump();
+}
+
+
+struct Arena<'arena> {
+    things: FrozenVec<Box<Thing<'arena>>>,
+}
+
+struct Thing<'arena> {
+    pub friends: FrozenVec<ThingRef<'arena>>,
+    pub reverse_friends: FrozenVec<ThingRef<'arena>>,
+    pub name: &'static str,
+}
+
+type ThingRef<'arena> = &'arena Thing<'arena>;
+
+
+impl<'arena> Arena<'arena> {
+    fn new() -> Arena<'arena> {
+        Arena {
+            things: FrozenVec::new(),
+        }
+    }
+    
+    fn add_thing(&'arena self, name: &'static str, friends: Vec<ThingRef<'arena>>) -> ThingRef<'arena> {
+        let idx = self.things.len();
+        self.things.push(Box::new(Thing {
+            name,
+            friends: friends.into(),
+            reverse_friends: FrozenVec::new(),
+        }));
+        let me = &self.things[idx];
+        for friend in &me.friends {
+            friend.reverse_friends.push(me)
+        }
+        me
+    }
+
+    fn dump(&'arena self) {
+        for thing in &self.things {
+            println!("friends of {}:", thing.name);
+            println!("\tdirect friends:");
+            for friend in &thing.friends {
+                println!("\t\t{}", friend.name);
+            }
+            println!("\treverse friends:");
+            for friend in &thing.reverse_friends {
+                println!("\t\t{}", friend.name);
+            }
+        }
+    }
+}
