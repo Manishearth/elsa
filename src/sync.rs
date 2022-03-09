ae64294b4ed408af417d3b5db35f5725fd5bf11f
Robert Bastian <robertbastian@users.noreply.github.com>
2022-03-09 15:03:11 +0100

    FrozenBTreeMap

--- src/sync.rs
+++ src/sync.rs
@@ -7,6 +7,7 @@
 //!
 
 use std::borrow::Borrow;
+use std::collections::BTreeMap;
 use std::collections::HashMap;
 use std::hash::Hash;
 
@@ -151,4 +152,55 @@ impl<T: StableDeref> FrozenVec<T> {
     }
 
     // TODO add more
+}
+
+/// Append-only threadsafe version of `std::collections::BTreeMap` where
+/// insertion does not require mutable access
+#[derive(Debug)]
+pub struct FrozenBTreeMap<K: Clone + Ord, V: StableDeref>(RwLock<BTreeMap<K, V>>);
+
+impl<K: Clone + Ord, V: StableDeref> FrozenBTreeMap<K, V> {
+    pub fn new() -> Self {
+        Self(RwLock::new(BTreeMap::new()))
+    }
+
+    /// Gets the item for the given key, or, if it doesn't exist, uses
+    /// the closure to create, insert, and return a reference to it.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use elsa::sync::FrozenBTreeMap;
+    ///
+    /// let map = FrozenBTreeMap::new();
+    /// assert_eq!(map.get_or_insert(&1, || -> Result<String,()> { Ok(String::from("a")) }), Ok("a"));
+    /// assert_eq!(map.get_or_insert(&1, || -> Result<String,()> { panic!() }), Ok("a"));
+    /// ```
+    pub fn get_or_insert<'a, E>(
+        &'a self,
+        k: &K,
+        value_fn: impl FnOnce() -> Result<V, E>,
+    ) -> Result<&'a V::Target, E> {
+        // The RHS of an if-let statement doesn't get dropped before the else block, so we have
+        // to do this manually
+        let guard = self.0.read().unwrap();
+        let ptr = if let Some(x) = guard.get(k) {
+            &**x as *const V::Target
+        } else {
+            drop(guard);
+
+            // We compute the value outside the or_insert_with because
+            // * it can fail
+            // * we don't want to hold the lock while computing it
+            let value = value_fn()?;
+
+            let mut guard = self.0.write().unwrap();
+
+            &**guard.entry(k.clone()).or_insert_with(|| value) as *const V::Target
+        };
+
+        // Even though we've given up the lock on it, the value is still there
+        // because V is a stable reference and we never drop anything.
+        Ok(unsafe { &*ptr })
+    }
 }
