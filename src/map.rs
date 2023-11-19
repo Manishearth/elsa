2c284f4c539efde143beb8dda5d0ad66ae056630
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-11-18 21:57:00 -0800

    feat: implement PartialEq for FrozenBTreeMap

--- src/map.rs
+++ src/map.rs
@@ -283,6 +283,19 @@ impl<K: Clone, V: Clone, S: Clone> Clone for FrozenMap<K, V, S> {
     }
 }
 
+impl<K: Eq + Hash, V: PartialEq + StableDeref> PartialEq for FrozenMap<K, V> {
+    fn eq(&self, other: &Self) -> bool {
+        assert!(!self.in_use.get());
+        assert!(!other.in_use.get());
+        self.in_use.set(true);
+        other.in_use.set(true);
+        let ret = self.map.get() == other.map.get();
+        self.in_use.set(false);
+        other.in_use.set(false);
+        ret
+    }
+}
+
 /// Append-only version of `std::collections::BTreeMap` where
 /// insertion does not require mutable access
 pub struct FrozenBTreeMap<K, V> {
@@ -522,7 +535,7 @@ impl<K: Clone, V: Clone> Clone for FrozenBTreeMap<K, V> {
     }
 }
 
-impl<K: Eq + Hash, V: PartialEq + StableDeref> PartialEq for FrozenMap<K, V> {
+impl<K: Eq + Hash, V: PartialEq + StableDeref> PartialEq for FrozenBTreeMap<K, V> {
     fn eq(&self, other: &Self) -> bool {
         assert!(!self.in_use.get());
         assert!(!other.in_use.get());
